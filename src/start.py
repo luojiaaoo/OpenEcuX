06c5fede15ad5c737c86531213e0b56425625391
NOKIAO <675925864@qq.com>
2025-05-16 16:06:40 +0800

    update

--- src/start.py
+++ src/start.py
@@ -3,5 +3,14 @@ import webview
 
 
 if __name__ == "__main__":
-    webview.create_window("Flask example", app.server)
+    webview.create_window(
+        "OpenEcuX",
+        app.server,
+        confirm_close=True,
+        width=600,
+        height=200,
+        focus=True,
+        resizable=False,
+        shadow=True,
+    )
     webview.start()
--- src/views/navigator.py
+++ src/views/navigator.py
@@ -1,2 +1,20 @@
+import feffery_antd_components as fac
+
+
 def render_content():
-    return 'Hello World!'
+    return fac.AntdSpace(
+        [
+            fac.AntdTabs(
+                items=[
+                    {
+                        "key": f"硬件",
+                        "label": f"硬件",
+                        "children": fac.AntdCard(),
+                    },
+                ],
+                size="small",
+            ),
+        ],
+        direction="vertical",
+        style={"width": "100%"},
+    )
